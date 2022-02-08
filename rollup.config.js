29efa2a6bdf66002003cc3bb576e28152c29b1f2
dashty94 <dashty.fr@gmail.com>
2022-02-08 12:52:41 +0300

    Add storybook

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,9 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 
+import { terser } from "rollup-plugin-terser";
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+
 const packageJson = require("./package.json");
 
 export default [
@@ -21,9 +24,13 @@ export default [
       },
     ],
     plugins: [
+        peerDepsExternal(),
+
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
+
+      terser()
     ],
   },
   {
