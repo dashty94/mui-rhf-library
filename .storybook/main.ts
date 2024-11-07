2e506db4b9e001ce9402ff5701a4b57bd7af0348
dashty94 <dashty.fr@gmail.com>
2024-11-07 16:16:15 +0300

    chore(storybook): update configuration and add new addons

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,14 +2,22 @@ import { StorybookConfig } from '@storybook/react-webpack5';
 
 const config: StorybookConfig = {
     stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-    addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+    addons: [
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        '@storybook/addon-webpack5-compiler-babel',
+        '@chromatic-com/storybook'
+    ],
+
     framework: {
         name: '@storybook/react-webpack5',
         options: {}
     },
 
-    docs: {
-        autodocs: true
+    docs: {},
+
+    typescript: {
+        reactDocgen: 'react-docgen-typescript'
     }
 };
 
